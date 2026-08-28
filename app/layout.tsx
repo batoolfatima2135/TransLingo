c7775c851492866afc7dd9dedc93d1e7f0bb8cbd
Batool Fatima <batoolfatima2135@gmail.com>
2026-08-28 22:48:07 +0200

    added layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
+import Navbar from "@/Components/Landing/Navbar";
+import Footer from "@/Components/Landing/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +26,14 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      {" "}
+      <Providers>
+        <body className="min-h-full flex flex-col">
+          <Navbar />
+          {children}
+          <Footer />
+        </body>
+      </Providers>
     </html>
   );
 }
